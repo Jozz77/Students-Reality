d9afeb024e553f7766eba667609852c0379aebc7
Jozz77 <mmadujonathan@gmail.com>
2024-02-12 18:35:53 +0100

    new

--- src/app/dashboard/students/page.jsx
+++ src/app/dashboard/students/page.jsx
@@ -33,7 +33,7 @@ export default function students() {
 
   return (
     <DashboardLayout>
-      <main className="overflow-x-auto bg-NormalWhite  rounded-l-[0px] sm:rounded-l-[40px] h-[100vh] pt-6 xxl:pt-8 px-[3%] ">
+      <main className="overflow-x-auto bg-NormalWhite  rounded-l-[0px] sm:rounded-l-[40px] h-[100vh] pt-6 xxl:pt-8 px-[5%] sm:px-[3%] ">
         <section className="flex justify-between items-center sm:hidden">
           <div onClick={toggleMenu}>
             {isOpen ? (
@@ -41,7 +41,7 @@ export default function students() {
                 <MdOutlineClose />
               </div>
             ) : (
-              <div className=" text-[2rem] ">
+              <div className=" text-[1.7rem] ">
                 <GiHamburgerMenu />
               </div>
             )}
@@ -58,8 +58,11 @@ export default function students() {
 
           {/* mobile nav starts */}
         {isOpen && (
-          <div className=" block sm:hidden absolute top-100 left-0 bottom-0 px-[2%] w-[50%] bg-ButtonColor h-[100vh] ">
-          <section className=" relative text-[0.7rem] md:text-[0.8rem] lg:text-[0.9rem] xxl:text-base text-NormalWhite mt-12 xxl:mt-16 ">
+          <div className=" block sm:hidden absolute z-10 left-0 bottom-0 px-[2%] w-[50%] bg-ButtonColor h-[100vh] ">
+            <div onClick={toggleMenu} className=" text-NormalWhite mt-2 text-[2rem] ">
+                <MdOutlineClose />
+              </div>
+          <section className=" relative text-[0.7rem] md:text-[0.8rem] lg:text-[0.9rem] xxl:text-base text-NormalWhite mt-8 xxl:mt-16 ">
             <input
               type="text"
               className=" rounded-[6px] w-full outline-none py-1 lg:py-2 pl-[20%] lg:pl-[15%]  bg-Gray700 placeholder:text-NormalWhite "
@@ -126,17 +129,17 @@ export default function students() {
 
         
 
-        <section className=" border-Gray200 border-t-[1px] sm:border-none pt-2 sm:pt-0 mt-3 sm:mt-0 flex justify-between items-center">
+        <section className=" border-Gray200 border-t-[1px] sm:border-none pt-2 sm:pt-0 mt-3 sm:mt-0 flex flex-col sm:flex-row justify-between items-start sm:items-center">
           <div>
-            <h1 className=" font-semibold text-[1.2rem] sm:text-[1.4rem] md:text-[1.5rem] lg:text-[1.7rem] xxl:text-[1.9rem] ">
+            <h1 className=" font-semibold text-[1.4rem] sm:text-[1.4rem] md:text-[1.5rem] lg:text-[1.7rem] xxl:text-[1.9rem] ">
               Students
             </h1>
-            <p className=" text-Gray600 text-[0.7rem] w-[90%] sm:w-full sm:text-[0.75rem] md:text-[0.8rem] lg:text-[0.9rem]  xxl:text-base">
+            <p className=" text-Gray600  text-[0.8rem] w-[90%] sm:w-full sm:text-[0.75rem] md:text-[0.8rem] lg:text-[0.9rem]  xxl:text-base">
               View and manage all your students from this portal
             </p>
           </div>
           <div className=" text-[0.65rem] sm:text-[0.7rem] md:text-[0.75rem] xl:text-[0.8rem] xxl:text-[0.9rem] items-center font-medium cursor-pointer flex gap-2 rounded-[6px] bg-ButtonColor text-NormalWhite py-2 px-2 sm:px-2 md:px-3 xl:px-4 ">
-            <div className=" text-[0.9rem] md:text-[1rem] xl:text-[1.1rem]">
+            <div className=" mt-3 sm:mt-0 text-[0.9rem] md:text-[1rem] xl:text-[1.1rem]">
               <FiPlus />
             </div>
             <p>Add Students</p>
@@ -194,7 +197,7 @@ export default function students() {
             </section>
           </div>
           <div className="overflow-x-auto ">
-            <table className=" table-auto  w-full text-left text-Gray500 text-[0.7rem] md:text-[0.7rem] lg:text-[0.75rem] xl:text-[0.8rem]">
+            <table className=" table-auto  w-full text-left text-Gray500 text-[0.7rem] md:text-[0.7rem] lg:text-[0.85rem] xl:text-[0.9rem]">
               <thead className=" bg-Gray50    font-medium  ">
                 <tr className=" justify-around border-Gray200 border-t-[1px] border-solid  ">
                   <th className=" flex gap-2 ml-[6%] py-2 ">
@@ -261,19 +264,19 @@ export default function students() {
             <button className="py-2 px-3 md:px-4 text-center bg-Gray50 border-Gray200 border-r-[1px] border-solid">
               1
             </button>
-            <button className="py-2 px-3 md:px-4 text-center border-Gray200 border-r-[1px] border-solid">
+            <button className="py-2 px-3 hidden sm:block md:px-4 text-center border-Gray200 border-r-[1px] border-solid">
               2
             </button>
-            <button className="py-2 px-3 md:px-4 text-center border-Gray200 border-r-[1px] border-solid">
+            <button className="py-2 hidden sm:block px-3 md:px-4 text-center border-Gray200 border-r-[1px] border-solid">
               3
             </button>
             <button className="py-2 px-3 md:px-4 text-center border-Gray200 border-r-[1px] border-solid">
               ...
             </button>
-            <button className="py-2 px-3 md:px-4 text-center border-Gray200 border-r-[1px] border-solid">
+            <button className="py-2 hidden sm:block px-3 md:px-4 text-center border-Gray200 border-r-[1px] border-solid">
               8
             </button>
-            <button className="py-2 px-3 md:px-4 text-center border-Gray200 border-r-[1px] border-solid">
+            <button className="py-2 px-3 hidden sm:block md:px-4 text-center border-Gray200 border-r-[1px] border-solid">
               9
             </button>
             <button className="py-2 px-2 md:py-3 text-center border-Gray200 border-r-[1px] border-solid">
