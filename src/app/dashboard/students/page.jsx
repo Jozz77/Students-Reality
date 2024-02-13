50f035b140d235c704bdb2aa6f870f587726e0ae
Jozz77 <mmadujonathan@gmail.com>
2024-02-13 10:28:44 +0100

    new

--- src/app/dashboard/students/page.jsx
+++ src/app/dashboard/students/page.jsx
@@ -15,6 +15,12 @@ import { RiHome6Line } from "react-icons/ri";
 import Image from "next/image";
 import Avatar from "../assets/Avatar.png";
 import Link from "next/link";
+import courses from "../assets/courses.png";
+import students1 from "../assets/students.png";
+import wallets from "../assets/wallets.png";
+import home from "../assets/home.png";
+import support from "../assets/support.png";
+import settings from "../assets/settings.png";
 
 export default function students() {
   const [user, setUser] = useState("");
@@ -57,77 +63,78 @@ export default function students() {
           </div>
 
           {/* mobile nav starts */}
-        {isOpen && (
-          <div className=" block sm:hidden absolute z-10 left-0 bottom-0 px-[2%] w-[50%] bg-ButtonColor h-[100vh] ">
-            <div onClick={toggleMenu} className=" text-NormalWhite mt-2 text-[2rem] ">
+          {isOpen && (
+            <div className=" block sm:hidden absolute z-10 left-0 bottom-0 px-[5%] w-[50%] bg-ButtonColor h-[100vh] ">
+              <div
+                onClick={toggleMenu}
+                className=" text-NormalWhite mt-8 text-[2rem] "
+              >
                 <MdOutlineClose />
               </div>
-          <section className=" relative text-[0.7rem] md:text-[0.8rem] lg:text-[0.9rem] xxl:text-base text-NormalWhite mt-8 xxl:mt-16 ">
-            <input
-              type="text"
-              className=" rounded-[6px] w-full outline-none py-1 lg:py-2 pl-[20%] lg:pl-[15%]  bg-Gray700 placeholder:text-NormalWhite "
-              name=""
-              id=""
-              placeholder="Search"
-            />
-            <div className=" absolute left-[5%] lg:left-[3%] top-[50%] translate-y-[-50%] ">
-              <FiSearch />
+              <section className=" relative text-[0.9rem] text-NormalWhite mt-8 ">
+                <input
+                  type="text"
+                  className=" rounded-[6px] w-full outline-none py-1 lg:py-2 pl-[20%] lg:pl-[15%]  bg-Gray700 placeholder:text-NormalWhite "
+                  name=""
+                  id=""
+                  placeholder="Search"
+                />
+                <div className=" absolute left-[5%] lg:left-[3%] top-[50%] translate-y-[-50%] ">
+                  <FiSearch />
+                </div>
+              </section>
+
+              <section className=" mt-4 flex flex-col py-4 gap-6 text-[0.9rem] text-Gray100 font-semibold">
+                <Link href="/dashboard" className=" flex gap-2 items-center ">
+                  <div className=" text-[1.1rem] ">
+                    <Image src={home} alt="Home Icon" />
+                  </div>
+                  <span>Home</span>
+                </Link>
+                <Link href="/dashboard" className=" flex gap-2 items-center ">
+                  <div className=" text-[1.1rem] ">
+                    <Image src={courses} alt="Courses Icon" />
+                  </div>
+                  <span>Courses</span>
+                </Link>
+                <Link
+                  href="/dashboard/students"
+                  className=" flex gap-2 items-center "
+                >
+                  <div className=" text-[1.1rem] ">
+                    <Image src={students1} alt="Students Icon" />
+                  </div>
+                  <span>Students</span>
+                </Link>
+                <Link href="/dashboard" className=" flex gap-2 items-center ">
+                  <div className=" text-[1.1rem] ">
+                    <Image src={wallets} alt="Wallets Icon" />
+                  </div>
+                  <span>Wallet</span>
+                </Link>
+              </section>
+
+              {/* down part */}
+              <div className=" absolute bottom-[5%] text-[0.9rem] flex flex-col gap-5 ">
+                <section className=" mt-10 flex flex-col gap-4 text-Gray100 font-semibold">
+                  <Link href="/dashboard" className=" flex gap-2 items-center ">
+                    <div className=" text-[1.1rem] ">
+                      <Image src={support} alt="Support Icon" />
+                    </div>
+                    <span>Support</span>
+                  </Link>
+                  <Link href="/dashboard" className=" flex gap-2 items-center ">
+                    <div className=" text-[1.1rem] ">
+                      <Image src={settings} alt="Settings" />
+                    </div>
+                    <span>Settings</span>
+                  </Link>
+                </section>
+              </div>
             </div>
-          </section>
-
-          <section className=" mt-4 flex flex-col py-4 gap-4 text-[0.8rem] lg:text-[0.9rem] xxl:text-base text-Gray100 font-semibold">
-            <Link href="/dashboard" className=" flex gap-2 items-center ">
-              <div className=" text-[1.1rem] ">
-                <RiHome6Line />
-              </div>
-              <span>Home</span>
-            </Link>
-            <Link href="/dashboard" className=" flex gap-2 items-center ">
-              <div className=" text-[1.1rem] ">
-                <RiHome6Line />
-              </div>
-              <span>Courses</span>
-            </Link>
-            <Link
-              href="/dashboard/students"
-              className=" flex gap-2 items-center "
-            >
-              <div className=" text-[1.1rem] ">
-                <RiHome6Line />
-              </div>
-              <span>Students</span>
-            </Link>
-            <Link href="/dashboard" className=" flex gap-2 items-center ">
-              <div className=" text-[1.1rem] ">
-                <RiHome6Line />
-              </div>
-              <span>Wallet</span>
-            </Link>
-          </section>
-
-          {/* down part */}
-          <div className=" absolute bottom-[5%] text-[0.7rem] md:text-[0.8rem] lg:text-[0.95rem] xxl:text-base flex flex-col gap-5 xxl:gap-6 ">
-            <section className=" mt-10 flex flex-col gap-4 text-Gray100 font-semibold">
-              <Link href="/dashboard" className=" flex gap-2 items-center ">
-                <div className=" text-[1.1rem] ">
-                  <RiHome6Line />
-                </div>
-                <span>Support</span>
-              </Link>
-              <Link href="/dashboard" className=" flex gap-2 items-center ">
-                <div className=" text-[1.1rem] ">
-                  <RiHome6Line />
-                </div>
-                <span>Settings</span>
-              </Link>
-            </section>
-          </div>
-        </div>
-        )}
-        {/* mobile nav ends */}
+          )}
+          {/* mobile nav ends */}
         </section>
-
-        
 
         <section className=" border-Gray200 border-t-[1px] sm:border-none pt-2 sm:pt-0 mt-3 sm:mt-0 flex flex-col sm:flex-row justify-between items-start sm:items-center">
           <div>
@@ -138,15 +145,15 @@ export default function students() {
               View and manage all your students from this portal
             </p>
           </div>
-          <div className=" text-[0.65rem] sm:text-[0.7rem] md:text-[0.75rem] xl:text-[0.8rem] xxl:text-[0.9rem] items-center font-medium cursor-pointer flex gap-2 rounded-[6px] bg-ButtonColor text-NormalWhite py-2 px-2 sm:px-2 md:px-3 xl:px-4 ">
-            <div className=" mt-3 sm:mt-0 text-[0.9rem] md:text-[1rem] xl:text-[1.1rem]">
+          <div className=" text-[0.85rem] mt-3 sm:mt-0 sm:text-[0.7rem] md:text-[0.75rem] xl:text-[0.8rem] xxl:text-[0.9rem] items-center font-medium cursor-pointer flex gap-2 rounded-[6px] bg-ButtonColor text-NormalWhite py-2 px-4 sm:px-2 md:px-3 xl:px-4 ">
+            <div className="  text-[0.9rem] md:text-[1rem] xl:text-[1.1rem]">
               <FiPlus />
             </div>
             <p>Add Students</p>
           </div>
         </section>
 
-        <section className=" flex justify-between mt-4 sm:mt-6 xxl:mt-8 font-medium text-[0.8rem] sm:text-[0.85rem] xl:text-[0.95rem] xxl:text-base">
+        <section className=" flex justify-between mt-4 sm:mt-6 xxl:mt-8 font-medium text-[0.9rem]">
           <div className=" w-[49%] border-Gray200 border-[1px] text-[0.8rem] sm:text-[0.85rem] border-solid rounded-[12px] py-2 sm:py-3 md:py-4 px-[2%] ">
             <small>Total Students</small>
             <h3 className=" mt-1 xxl:mt-4 font-semibold text-[1.2rem] sm:text-[1.4rem] md:text-[1.6rem] lg:text-[1.8rem] xl:text-[2rem] xxl:text-[2.2rem] ">
@@ -162,95 +169,95 @@ export default function students() {
         </section>
 
         <section className="mt-4 sm:mt-6  overflow-x-auto xxl:mt-8 border-Gray200 shadow-sm border-[1px] border-solid  rounded-[8px] ">
-         <div className="min-w-[550px]">
-         <div className=" flex justify-between py-2 xxl:py-3 px-[2%] ">
-            <section className=" flex justify-between  lg:w-[35%] w-[45%] md:w-[40%] xl:w-[30%] text-Gray700 text-[0.7rem] sm:text-[0.7rem] md:text-[0.75rem] lg:text-[0.8rem] xxl:text-[0.9rem] font-medium  rounded-[8px] border-Gray200 border-[1px] border-solid">
-              <span className="text-center py-1 sm:py-2 w-[33.3%]">
-                View all
-              </span>
-              <span className="w-[33.3%] text-center py-1 sm:py-2 border-Gray200 border-x-[1px] border-solid justify-self-center">
-                cohort
-              </span>
-              <span className="w-[33.3%] text-center py-1 sm:py-2">
-                course type
-              </span>
-            </section>
-            <section className=" flex items-center justify-between w-[46%] ">
-              <div className=" relative w-[70%] sm:w-[78%] text-[0.6rem] sm:text-[0.65rem] md:text-[0.7rem] lg:text-[0.8rem] xxl:text-[0.9rem] xxl:text-base ">
-                <input
-                  type="text"
-                  className=" rounded-[6px] w-full outline-none py-1 sm:py-1 pl-[13%] md:pl-[10%] border-Gray200 border-[1px] border-solid  "
-                  name=""
-                  id=""
-                  placeholder="Search for students"
-                />
-                <div className=" absolute left-[3%] top-[50%] translate-y-[-50%] ">
-                  <FiSearch />
+          <div className="min-w-[550px]">
+            <div className=" flex justify-between py-2 xxl:py-3 px-[2%] ">
+              <section className=" flex justify-between  lg:w-[35%] w-[45%] md:w-[40%] xl:w-[30%] text-Gray700 text-[0.7rem] sm:text-[0.7rem] md:text-[0.75rem] lg:text-[0.8rem] xxl:text-[0.9rem] font-medium  rounded-[8px] border-Gray200 border-[1px] border-solid">
+                <span className="text-center py-1 sm:py-2 w-[33.3%]">
+                  View all
+                </span>
+                <span className="w-[33.3%] text-center py-1 sm:py-2 border-Gray200 border-x-[1px] border-solid justify-self-center">
+                  cohort
+                </span>
+                <span className="w-[33.3%] text-center py-1 sm:py-2">
+                  course type
+                </span>
+              </section>
+              <section className=" flex items-center justify-between w-[46%] ">
+                <div className=" relative w-[70%] sm:w-[78%] text-[0.6rem] sm:text-[0.65rem] md:text-[0.7rem] lg:text-[0.8rem] xxl:text-[0.9rem] xxl:text-base ">
+                  <input
+                    type="text"
+                    className=" rounded-[6px] w-full outline-none py-1 sm:py-1 pl-[13%] md:pl-[10%] border-Gray200 border-[1px] border-solid  "
+                    name=""
+                    id=""
+                    placeholder="Search for students"
+                  />
+                  <div className=" absolute left-[3%] top-[50%] translate-y-[-50%] ">
+                    <FiSearch />
+                  </div>
                 </div>
-              </div>
-              <div className="border-Gray200 w-[28%] sm:w-[20%] py-1 border-[1px] text-Gray700 text-[0.6rem] sm:text-[0.7rem] md:text-[0.75rem] lg:text-[0.8rem] xxl:text-[0.9rem] rounded-[6px] border-solid  items-center justify-center flex gap-2">
-                <div className=" text-[0.9rem] md:text-[1rem] lg:text-[1.1rem] ">
-                  <IoFilterSharp />
+                <div className="border-Gray200 w-[28%] sm:w-[20%] py-1 border-[1px] text-Gray700 text-[0.6rem] sm:text-[0.7rem] md:text-[0.75rem] lg:text-[0.8rem] xxl:text-[0.9rem] rounded-[6px] border-solid  items-center justify-center flex gap-2">
+                  <div className=" text-[0.9rem] md:text-[1rem] lg:text-[1.1rem] ">
+                    <IoFilterSharp />
+                  </div>
+                  <p>Filter</p>
                 </div>
-                <p>Filter</p>
-              </div>
-            </section>
-          </div>
-          <div className="overflow-x-auto ">
-            <table className=" table-auto  w-full text-left text-Gray500 text-[0.7rem] md:text-[0.7rem] lg:text-[0.85rem] xl:text-[0.9rem]">
-              <thead className=" bg-Gray50    font-medium  ">
-                <tr className=" justify-around border-Gray200 border-t-[1px] border-solid  ">
-                  <th className=" flex gap-2 ml-[6%] py-2 ">
-                    <input type="checkbox" name="" id="checkbox" />
-                    <label htmlFor="checkbox">Description</label>
-                  </th>
-                  <th className=" ">Course</th>
-                  <th>Date Joined</th>
-                  <th className="  pl-2 sm:pl-4 lg:pl-6 ">Cohort</th>
-                  <th className="  pl-2 sm:pl-4 lg:pl-6 ">Status</th>
-                  <th className="  pl-10 lg:pl-16 "></th>
-                </tr>
-              </thead>
-              <tbody className=" ">
-                {StudentsData.map((student) => (
-                  <tr
-                    key={student.id}
-                    className="border-Gray200 border-t-[1px] border-solid justify-between "
-                  >
-                    <td className="px-[6%]  text-Gray900 py-4 flex gap-2">
-                      <input type="checkbox" name="" id={student.id} />
-                      <label htmlFor={student.id} className="font-medium">
-                        {student.name}
-                      </label>
-                    </td>
-                    <td>{student.course}</td>
-                    <td>{student.date}</td>
-                    <td className="  pl-2 sm:pl-4 lg:pl-6 ">
-                      {student.cohort}
-                    </td>
-                    <td className="relative pl-2 sm:pl-4 lg:pl-6 ">
-                      <div
-                        className={` absolute flex gap-1 py-1 px-2 top-[50%] translate-y-[-50%] items-center rounded-[16px] ${
-                          student.status === "Inactive"
-                            ? "bg-Warning50 text-Warning700 "
-                            : "bg-[#ECFDF3] text-[#049B77] "
-                        }`}
-                      >
-                        <div className=" text-[0.9rem] ">
-                          <IoMdCheckmark />
-                        </div>
-                        {student.status}
-                      </div>
-                    </td>
-                    <td className=" text-[1.2rem] pl-10 lg:pl-16 ">
-                      <BiDotsVerticalRounded />
-                    </td>
+              </section>
+            </div>
+            <div className="overflow-x-auto ">
+              <table className=" table-auto  w-full text-left text-Gray500 text-[0.7rem] md:text-[0.7rem] lg:text-[0.85rem] xl:text-[0.9rem]">
+                <thead className=" bg-Gray50    font-medium  ">
+                  <tr className=" justify-around border-Gray200 border-t-[1px] border-solid  ">
+                    <th className=" flex gap-2 ml-[6%] py-2 ">
+                      <input type="checkbox" name="" id="checkbox" />
+                      <label htmlFor="checkbox">Description</label>
+                    </th>
+                    <th className=" ">Course</th>
+                    <th>Date Joined</th>
+                    <th className="  pl-2 sm:pl-4 lg:pl-6 ">Cohort</th>
+                    <th className="  pl-2 sm:pl-4 lg:pl-6 ">Status</th>
+                    <th className="  pl-10 lg:pl-16 "></th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody className=" ">
+                  {StudentsData.map((student) => (
+                    <tr
+                      key={student.id}
+                      className="border-Gray200 border-t-[1px] border-solid justify-between "
+                    >
+                      <td className="px-[6%]  text-Gray900 py-4 flex gap-2">
+                        <input type="checkbox" name="" id={student.id} />
+                        <label htmlFor={student.id} className="font-medium">
+                          {student.name}
+                        </label>
+                      </td>
+                      <td>{student.course}</td>
+                      <td>{student.date}</td>
+                      <td className="  pl-2 sm:pl-4 lg:pl-6 ">
+                        {student.cohort}
+                      </td>
+                      <td className="relative pl-2 sm:pl-4 lg:pl-6 ">
+                        <div
+                          className={` absolute flex gap-1 py-1 px-2 top-[50%] translate-y-[-50%] items-center rounded-[16px] ${
+                            student.status === "Inactive"
+                              ? "bg-Warning50 text-Warning700 "
+                              : "bg-[#ECFDF3] text-[#049B77] "
+                          }`}
+                        >
+                          <div className=" text-[0.9rem] ">
+                            <IoMdCheckmark />
+                          </div>
+                          {student.status}
+                        </div>
+                      </td>
+                      <td className=" text-[1.2rem] pl-10 lg:pl-16 ">
+                        <BiDotsVerticalRounded />
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
-         </div>
         </section>
 
         <section className="mt-6 sm:mt-8 xxl:mt-10 relative mb-2 sm:mb-16 pb-6 ">
